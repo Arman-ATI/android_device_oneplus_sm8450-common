3c8e58d74213775310cc4704e1001799d29eb854
ralph950412 <ralph950412@gmail.com>
2026-06-24 20:12:31 +0900

    sm8450-common: Patch libwfdservice to load android.media.audio.common.types-V5-cpp

--- extract-files.py
+++ extract-files.py
@@ -69,6 +69,8 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libprotobuf-cpp-full.so', 'libprotobuf-cpp-full-21.7.so'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .add_needed('libinput_shim.so'),
+    'system_ext/lib64/libwfdservice.so': blob_fixup()
+        .replace_needed('android.media.audio.common.types-V4-cpp.so', 'android.media.audio.common.types-V5-cpp.so'),
     'vendor/bin/qguard': blob_fixup()
         .add_needed('libbase_shim.so'),
     'vendor/bin/hw/vendor.qti.hardware.display.composer-service': blob_fixup()
