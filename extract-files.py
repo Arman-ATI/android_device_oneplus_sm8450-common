184f23d0eb2e481c1a11c9368fce64e78969861d
binbin323 <bin@dhao.cc>
2026-06-24 20:21:40 +0900

    sm8450-common: Patch libwfdmmsrc_system with libgui_shim

--- extract-files.py
+++ extract-files.py
@@ -69,6 +69,8 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libprotobuf-cpp-full.so', 'libprotobuf-cpp-full-21.7.so'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .add_needed('libinput_shim.so'),
+    'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
+        .add_needed('libgui_shim.so'),
     'system_ext/lib64/libwfdservice.so': blob_fixup()
         .replace_needed('android.media.audio.common.types-V4-cpp.so', 'android.media.audio.common.types-V5-cpp.so'),
     'vendor/bin/qguard': blob_fixup()
